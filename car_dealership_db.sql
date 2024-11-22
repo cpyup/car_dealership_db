65f278dc824f0a7e7ff50cce6bb57d6dda8733ac
Cameron <cpolly@pgh.yearup.org>
2024-11-22 12:34:18 -0500

    Populated With Data

--- car_dealership_db.sql
+++ car_dealership_db.sql
@@ -1,10 +1,11 @@
+-- Drop existing database if exists
 drop database if exists dealership;
 create database if not exists dealership;
 use dealership;
 
 -- Dealerships Table
 create table `dealerships` (
-	`dealership_id` integer not null auto_increment,
+    `dealership_id` integer not null auto_increment,
     `name` varchar(50),
     `address` varchar(50),
     `phone` varchar(12),
@@ -13,7 +14,7 @@ create table `dealerships` (
 
 -- Vehicles table
 create table `vehicles` (
-	`vin` integer not null,
+    `vin` varchar(20) not null,
     `year` integer not null,
     `make` varchar(20) not null,
     `model` varchar(20) not null,
@@ -27,18 +28,20 @@ create table `vehicles` (
 
 -- Inventory table
 create table `inventory`(
-	`dealership_id` integer not null,
-    `vin` integer not null,
-    constraint `pk_inventory` primary key(`dealership_id`)
+    `dealership_id` integer not null,
+    `vin` varchar(20) not null,
+    constraint `pk_inventory` primary key(`dealership_id`, `vin`),
+    constraint `fk_inventory_dealerships` foreign key (`dealership_id`) references `dealerships`(`dealership_id`),
+    constraint `fk_inventory_vehicles` foreign key (`vin`) references `vehicles`(`vin`)
 ); 
 
 -- Sales contracts table
 create table `sales_contracts`(
-    `contract_date` varchar(15) not null,
+    `contract_date` date not null,
     `name` varchar(50) not null,
     `email` varchar(50) not null,
     `id` integer not null auto_increment,
-    `vin` integer not null,
+    `vin` varchar(20) not null,
     `sales_tax` float not null,
     `recording_fee` float not null,
     `processing_fee` float not null,
@@ -51,11 +54,11 @@ create table `sales_contracts`(
 
 -- Lease contracts table
 create table `lease_contracts`(
-	`contract_date` varchar(15) not null,
+    `contract_date` date not null, 
     `name` varchar(50) not null,
     `email` varchar(50) not null,
-	`id` integer not null auto_increment,
-    `vin` integer not null,
+    `id` integer not null auto_increment,
+    `vin` varchar(20) not null,
     `ending_value` float not null,
     `lease_fee` float not null,
     `total_cost` float not null,
@@ -65,3 +68,26 @@ create table `lease_contracts`(
 );
 
 -- Add data to tables
+
+-- Dealership data
+insert into `dealerships`(`name`, `address`, `phone`) 
+    values('Car World', '123 Main St', '555-123-4567');
+
+-- Vehicles Data
+insert into `vehicles`(`vin`, `year`, `make`, `model`, `type`, `color`, `odometer`, `price`, `sold`)
+    values('10234', 2015, 'Toyota', 'Camry', 'Silver','Sedan', 12000, 15995.0, false),
+          ('10112', 1993, 'Ford', 'Explorer', 'Red','SUV', 525123, 995.0, true),
+          ('37846', 2021, 'Chevrolet', 'Silverado', 'Black', 'Sedan', 2750, 31995.0, true);
+          
+insert into `inventory`(`dealership_id`,`vin`)
+		values(1,'10234'),
+			  (1,'10112'),
+			  (1,'37846');
+
+-- Sales Contract Data
+insert into `sales_contracts`(`contract_date`, `name`, `email`, `vin`, `sales_tax`, `recording_fee`, `processing_fee`, `total_cost`, `financed`, `monthly_payment`)
+    values('2021-09-28', 'Dana Wyatt', 'dana@texas.com', '10112', 995.0, 49.75, 100.0, 295.0, false, 0.0);
+
+-- Lease Contract Data
+insert into `lease_contracts`(`contract_date`, `name`, `email`, `vin`, `ending_value`, `lease_fee`, `total_cost`, `monthly_payment`)
+    values('2021-09-28', 'Zachary Westly', 'zach@texas.com', '37846', 15997.5, 2239.65, 18237.15, 538.43);
