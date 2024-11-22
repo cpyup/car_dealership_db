a31a3e6a1f93b42a6bfe6b1325d3c053d62962e9
Cameron <cpolly@pgh.yearup.org>
2024-11-22 15:21:25 -0500

    Adjusted Layout

--- queries/get_sales_info_for_dealer_by_dates.sql
+++ queries/get_sales_info_for_dealer_by_dates.sql
@@ -1,8 +1,8 @@
 -- Sales info for a specific dealer for a range of dates
-SELECT *
-FROM dealerships AS d
+SELECT s.*
+FROM sales_contracts AS s
 INNER JOIN inventory AS i
+		ON s.vin = i.vin
+INNER JOIN dealerships AS d
 		ON i.dealership_id = d.dealership_id
-INNER JOIN sales_contracts AS s
-		ON i.vin = s.vin
 WHERE d.dealership_id = 1 AND s.contract_date BETWEEN '2020-01-01' AND '2021-12-31';
